11929450192b0034fc9aef7ee4007662cd15ca41
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-14 14:52:30 -0400

    Updated from Brython Server: 9/14/2017 2:52:29 PM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,22 @@
+#Erik Hansen
+#9/14/2017
+#unitConverter.py - converts units
 
+print('1) Kilometers to Miles')
+print('2) Kilograms to Pounds')
+print('3) Liters to Gallons')
+print('4) Celsius to Fahrenheit')
+
+opt = int(input('Choose a number: '))
+if opt == 1:
+    km = float(input('Enter number of Kilometers: '))
+    print(km, 'kilometers is',km*.621,'miles')
+elif opt == 2:
+    kg = float(input('Enter a number of Kilograms: '))
+    print(kg, 'kilograms is',kg*2.204,'pounds')
+elif opt == 3:
+    l = float(input('Enter a number of Liters: '))
+    print(l, 'liters is',l*.264,'gallons')
+else:
+    c = float(input('Enter degrees in Celcius: '))
+    print(c, 'degrees Celsius is',c*1.8+32,'degrees Fahrenheit')
