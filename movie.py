e60f64a48f2d952e09c6c82c990c3fad3ee64244
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-14 14:34:33 -0400

    Updated from Brython Server: 9/14/2017 2:34:32 PM

--- movie.py
+++ movie.py
@@ -1,1 +1,11 @@
+#Erik Hansen
+#9/14/2017
+#movies.py - most scandalous movie allowed
 
+age = int(input('Enter your age: '))
+if age >= 18:
+    print('You can watch rated R movies')
+elif age >= 13:
+    print('You can watch rated PG-13 movies')
+else:
+    print('You can watch rated PG movies')
