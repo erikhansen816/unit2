a266f11a2e3ff34dc831baeced900f0b21a45daa
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-15 14:54:18 -0400

    Updated from Brython Server: 9/15/2017 2:54:16 PM

--- adventure.py
+++ adventure.py
@@ -1,1 +1,6 @@
+#Erik Hansen
+#9/15/2017
+#adventure.py - making an adventure
 
+print('A bat bites you while you are sleeping and you wake up with supernatural agility and hate the light. You realize you are a vampire. After being a vampire for two days you start to crave blood. A human enters the cave to check on you')
+ans1 = input('Do you bite his neck, yes or no?')
