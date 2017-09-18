c2fe4967e639983937c190d94a23b069e56c2b0b
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-18 14:11:02 -0400

    Updated from Brython Server: 9/18/2017 2:10:58 PM

--- inDemo.py
+++ inDemo.py
@@ -1,1 +1,9 @@
+#Erik Hansen
+#9/18/2017
+#inDemo.py - how to use the in operator
 
+word = input('Enter a word: ')
+if 'w' in word or 'W' in word:
+    print(word, 'is cool')
+else:
+    print(word, 'is NOT cool')
