81a92b0a9fd07303e44ea24fd2e7d74f550315b1
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-25 14:24:59 -0400

    Updated from Brython Server: 9/25/2017 2:24:57 PM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,28 @@
+#Erik Hansen
+#9/25/2017
+#quiz2.py - Quiz program on unit 2
 
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a second number: '))
+#This part determines which number is the biggest
+if num1>num2:
+    print('The first number is bigger')
+elif num2>num1:
+    print('The second number is bigger')
+else:
+    print('The numbers are the same')
+#This part determines their divisibility by 3
+if num1%3 == 0 and num2%3 == 0:
+    print('Both are divisible by 3')
+elif num1%3 == 0:
+    print('The first is divisible by 3')
+elif num2%3 == 0:
+    print('The second is divisible by 3')
+else:
+    print('Neither are divisible by 3')
+#This part asks for the product and determines if the answer is correct
+product = int(input('What is the product of these two numbers? '))
+if product == num1*num2:
+    print('Correct')
+else:
+    print('Incorrect')
